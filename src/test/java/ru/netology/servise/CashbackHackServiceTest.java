7f2f6802b24a45324bc5c5388db6c96b9390026f
Galina Demidova <galinagriz77@gmail.com>
2022-07-19 21:34:13 +0300

    commit junit4

--- src/test/java/ru/netology/servise/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/servise/CashbackHackServiceTest.java
@@ -1,19 +1,57 @@
 package ru.netology.servise;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CashbackHackServiceTest {
-
+public class CashbackHackServiceTest {
     @Test
-    void remain() {
+    public void remain() {
         CashbackHackService service = new CashbackHackService();
-        int expected = 100;
-        int amount = 900;
+        int expected = 90;
+        int amount = 910;
         int result = service.remain(amount);
 
         assertEquals(expected,result);
 
     }
+    @Test
+    public void remain1() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int amount = 1000;
+        int result = service.remain(amount);
+
+        assertEquals(expected,result);
+
+    }
+    @Test
+    public void remain2() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int amount = 1900;
+        int result = service.remain(amount);
+
+        assertEquals(expected,result);
+
+    }
+    //@Test
+    public void remain3() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int amount = 1000;
+        int result = service.remain(amount);
+
+        assertEquals(expected,result);
+
+    }
+    @Test
+    public void remain4() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int amount = 0;
+        int result = service.remain(amount);
+
+        assertEquals(expected,result);
+
+    }
+
 }

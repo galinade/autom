94426f6b4c6e9f79aeeabef57f2ebcfcf35bfd23
Galina Demidova <galinagriz77@gmail.com>
2022-07-19 15:13:08 +0300

    commit testng

--- src/test/java/ru/netology/servise/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/servise/CashbackHackServiceTest.java
@@ -1,19 +1,23 @@
 package ru.netology.servise;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
-class CashbackHackServiceTest {
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
 
     @Test
     void remain() {
         CashbackHackService service = new CashbackHackService();
-        int expected = 100;
-        int amount = 900;
+        int expected = 200;
+        int amount = 800;
         int result = service.remain(amount);
 
-        assertEquals(expected,result);
+        assertEquals(result,expected);
 
     }
 }
